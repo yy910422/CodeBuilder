ab4fc76b8a9c1795d04b4d1542634459ab87fffb
yy910422 <yy9103@163.com>
2015-12-03 14:46:47 +0800

    add code
    moneybuilder

---
+++ MoneyBuilder/Common/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PetaPoco;
+using MonBuilder.Repositories;
+namespace MonBuilder.Common
+{
+    public class test
+    {
+        //private Database dao = CreateDAO();
+        private Database dao = Database.CreateInstance();
+
+        public void testDao()
+        {
+            
+        }
+    }
+}
